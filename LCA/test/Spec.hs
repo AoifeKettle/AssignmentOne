6b5be413312087d17a52d3be5e05e214eb4114bb
AoifeKettle <akettle@tcd.ie>
2018-09-29 18:54:11 +0100

    Haskell for Hello World with tests

--- LCA/test/Spec.hs
+++ LCA/test/Spec.hs
@@ -1,2 +1,21 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+
+{-# LANGUAGE StandaloneDeriving #-}
+
+module Main where
+
+import Test.HUnit
+import Test.Framework as TestFramework (defaultMain, testGroup, Test)
+import Test.Framework.Providers.HUnit (testCase)
+
+import Lib
+
+-- HUnit Tests
+
+test1 =  helloTest @?= "Hello World"
+
+-- QuickCheck Tests
+
+main = defaultMain tests
+
+tests :: [TestFramework.Test]
+tests = [ testGroup "Tests" [testCase "HelloWorld" test1]]
