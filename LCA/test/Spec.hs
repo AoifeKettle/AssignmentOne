264b63d11b7ea262b3daba64cf5956ab0cb9c9fc
AoifeKettle <akettle@tcd.ie>
2018-10-04 13:24:05 +0100

    New tests for command line arguement input

--- LCA/test/Spec.hs
+++ LCA/test/Spec.hs
@@ -9,17 +9,33 @@ import Test.Framework.Providers.HUnit (testCase)
 import Lib
 -- HUnit Tests
 
-testEmpty =  show_tree Empty 0 0 @?= False
-testNonEmpty =  show_tree tree1 0 0 @?= True
-testLCAEmpty = lca Empty 0 0 @?= Left False
-testLCANotPresent = lca tree1 0 9 @?= Left False
+testEmpty =  show_tree Empty [] @?= False
+testNonEmpty =  show_tree tree1 [1, 3] @?= True
+testLCAEmpty = lca Empty [] @?= Left False
+testLCANotPresent = lca tree1 [0, 9] @?= Left False
+testLCAPresent = lca tree1 [4, 5] @?= Right 2
 
 -- QuickCheck Tests
 
-main = defaultMain tests
+main = defaultMain testsBasic
 
-tests :: [TestFramework.Test]
-tests = [ testGroup "Tests" [testCase "emptyTree" testEmpty,
+testsBasic :: [TestFramework.Test]
+testsBasic = [ testGroup "BasicTestsSampleCode" [testCase "emptyTree" testEmpty,
                               testCase "nonEmptyTree" testNonEmpty,
-                                testCase "notPresentLCA" testLCANotPresent,
-                                testCase "LCAEmpty" testLCAEmpty]]
+                              testCase "notPresentLCA" testLCANotPresent,
+                              testCase "LCAEmpty" testLCAEmpty,
+                              testCase "LCAPresent" testLCAPresent]]
+
+
+testEmptyBuild = show_tree Empty [] @?= False
+testBuild = show_tree tree1 [1, 3] @?= True
+
+testBuildTree :: [TestFramework.Test]
+testBuildTree = [testGroup "Test tree build" [testCase "Build empty tree" testEmptyBuild,
+                                              testCase "Build full tree" testBuild]]
+testLCAcalcTrue = lca tree1 [4, 5] @?= Right 4
+testLCAcalcFalse = lca tree1 [6, 7] @?= Left False
+
+testLCAcalc :: [TestFramework.Test]
+testLCAcalc = [testGroup "Test tree build" [testCase "LCA not in tree" testLCAcalcTrue,
+                                              testCase "LCa in tree" testLCAcalcFalse]]
